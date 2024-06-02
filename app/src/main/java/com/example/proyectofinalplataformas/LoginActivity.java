6424a020ff5d8bf46f2de5311057e5dd5093574c
AxelAlmonte <aalmontecu@unsa.edu.pe>
2024-06-02 01:39:59 -0500

    creacion de fragments y activitys

--- app/src/main/java/com/example/proyectofinalplataformas/LoginActivity.java
+++ app/src/main/java/com/example/proyectofinalplataformas/LoginActivity.java
@@ -8,7 +8,7 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
-public class MainActivity extends AppCompatActivity {
+public class LoginActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
