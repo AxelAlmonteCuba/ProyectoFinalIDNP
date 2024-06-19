40a7c4d97ce762d3e0db22a9541836dd1243c1c6
Axel Almonte <91153693+AxelAlmonteCuba@users.noreply.github.com>
2024-06-19 00:38:10 -0500

    Update PinturasFragment.java

--- app/src/main/java/com/example/proyectofinalplataformas/fragments/PinturasFragment.java
+++ app/src/main/java/com/example/proyectofinalplataformas/fragments/PinturasFragment.java
@@ -60,7 +60,6 @@ public class PinturasFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        View view = inflater.inflate(R.layout.fragment_pinturas, container, false);
-        return view;
+        return inflater.inflate(R.layout.fragment_pinturas, container, false);        
     }
 }
