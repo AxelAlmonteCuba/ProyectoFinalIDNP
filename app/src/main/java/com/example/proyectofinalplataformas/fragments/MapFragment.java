33149c6256b0924b2daa4a2fd3f306133ba40e43
CayoCayoFiorella <83928594+CayoCayoFiorella@users.noreply.github.com>
2024-06-19 21:50:31 -0500

    Update MapFragment.java

--- app/src/main/java/com/example/proyectofinalplataformas/fragments/MapFragment.java
+++ app/src/main/java/com/example/proyectofinalplataformas/fragments/MapFragment.java
@@ -2,33 +2,23 @@ package com.example.proyectofinalplataformas.fragments;
 
 import android.os.Bundle;
 
-import android.os.Bundle;
-
 import androidx.fragment.app.Fragment;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
-import com.example.proyectofinalplataformas.R;
+import com.example.proyectofinalplataformas.MapView;
 
-/**
- * A simple {@link Fragment} subclass.
- * Use the {@link MapFragment#newInstance} factory method to
- * create an instance of this fragment.
- */
 public class MapFragment extends Fragment {
 
     private static final String ARG_PARAM1 = "param1";
     private static final String ARG_PARAM2 = "param2";
-
     private String mParam1;
     private String mParam2;
 
     public MapFragment() {
-        // Required empty public constructor
     }
-
     public static MapFragment newInstance(String param1, String param2) {
         MapFragment fragment = new MapFragment();
         Bundle args = new Bundle();
@@ -50,7 +40,6 @@ public class MapFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_map, container, false);
+        return new MapView(getContext());
     }
 }
