cc474ee5b91854ce537ef9e429281798e4ef0285
axel <aalmontecu@unsa.edu.pe>
2024-08-03 02:30:41 -0500

    correcciones

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -47,10 +47,6 @@ dependencies {
     implementation ("com.google.android.gms:play-services-auth:19.2.0")
     implementation(libs.room.common)
     implementation(libs.room.runtime)
-
-
-    testImplementation(libs.junit)
-    androidTestImplementation(libs.ext.junit)
-    androidTestImplementation(libs.espresso.core)
     annotationProcessor(libs.room.compiler)
+
 }
